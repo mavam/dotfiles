b716f2b2a84d330d5cb122c8c76d7bc42bfff34a
Matthias Vallentin <vallentin@icir.org>
2010-04-09 18:06:33 +0000

    [Mutt] Switching to Gmail's nested labels.

--- .mutt/scripts/build_mailboxes.rb
+++ .mutt/scripts/build_mailboxes.rb
@@ -1,25 +1,17 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 # Where to look for directories.
 MAILDIR = "#{ENV['HOME']}/.gmail"
 
-# Order of mailboxes (if available).
+# Custom order of mailboxes.
 ORDER = %w{
     INBOX
     sent
     drafts
     flagged
-    list.boost
-    list.boost.spirit
-    list.zsh
-    list.screen
-    list.sup-talk
-    list.pentest
-    list.metasploit
-    list.ids
-    list.wifisec
-    list.botnets
-    list.misc
+    list
     private
     devnull
     health
@@ -42,7 +34,38 @@ ORDER = %w{
     universe
 }
 
-dirs = Dir.entries(MAILDIR)
-mboxes = dirs.sort_by {|dir| ORDER.index(dir) || dirs.index(dir) + ORDER.size} 
+# Check whether a directory is a Maildir. 
+def maildir?(dir)
+  intersection = Dir.entries(dir).to_set & ["cur", "new", "tmp"]
+  return intersection.size == 3
+end
 
-puts mboxes.reject {|box| box[/^(\.){1,2}$/]}.map {|box| "\"=#{box}\""} * " "
+# Build mailboxes from a given base directory. 
+def build(base)
+  dirs = Dir.entries(base).reject { |dir| dir =~ /^\.{1,2}$/ }
+
+  mail, more = dirs.partition do |dir| 
+    path = File.join(base, dir)
+    xor = Dir.entries(path).to_set ^ [".", "..", "cur", "new", "tmp"].to_set
+    xor.size == 0
+  end
+
+  return mail if more.to_set == ["cur", "new", "tmp"].to_set
+
+  nested = more.map do |dir| 
+    boxes = build(File.join(base, dir))
+    [dir] + boxes.map { |box| File.join(dir,box) }
+  end
+
+  mail + nested
+end
+
+dirs = build(MAILDIR)
+
+# FIXME: prettify this ugliness.
+mboxes = dirs.sort_by do |mbox| 
+  box = mbox.kind_of?(Array) ? mbox[0] : mbox
+  ORDER.index(box) || ORDER.size + dirs.index(box)
+end
+
+puts mboxes.flatten.map {|box| "\"=#{box}\""} * " "
