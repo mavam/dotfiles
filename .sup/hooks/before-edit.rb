42a37bcc10cd6be3713be5c352ab33a4e858d838
Matthias Vallentin <vallentin@icir.org>
2011-07-19 08:16:45 -0700

    Fix edit hook.
    The hook cannot include method definition that bind variables from the global
    scope. Hence this workaround.

--- .sup/hooks/before-edit.rb
+++ .sup/hooks/before-edit.rb
@@ -1,22 +1,29 @@
-# Mutt-like send-hooks.
-recipients = header.values_at("To", "Cc", "Bcc").flatten
-if recipients.any? { |r| r =~ /sup-talk|lists\.|zsh-users/ }
-  a = Redwood::AccountManager.account_for("vallentin@icsi.berkeley.edu")
-  header["From"] = a.full_address
+# Mutt-like send-hooks. Select an account identified by it's full email address
+# given a pattern to be matched against the recipients.
+me = nil
+header.values_at("To", "Cc", "Bcc").flatten.each do |recipient|
+  case recipient
+  when /bro-ids.org/
+    me = "vallentin@icir.org"
+  when /sup-talk|lists\.|zsh-users/
+    me = "vallentin@icsi.berkeley.edu"
+  end
 end
+header["From"] = Redwood::AccountManager.account_for(me).full_address if me
+
 
 # Body signature.
 default_sig = ["", "    Matthias"]
-eecs_sig = default_sig + [""] + 
-  ["Matthias Vallentin", 
-   "EECS Department", 
+eecs_sig = default_sig + [""] +
+  ["Matthias Vallentin",
+   "EECS Department",
    "723 Soda Hall - MC 1776",
-   "University of California", 
+   "University of California",
    "Berkeley, CA, USA  94720-1776"]
 
 # TODO: add some logic that selects the appropriate signature.
 sig = default_sig
 
-# FIXME: The check whether the signature has already been appended is necssary
-# because the hook is executed multiple times per email. If have no clue why...
+# The check whether the signature has already been appended is necssary because
+# the hook is executed multiple times per email. No clue why...
 body.concat(sig) unless body.last == sig.last
