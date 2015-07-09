97fd77b55af2c17a4d376eeb64dcf4ad29f173f0
Matthias Vallentin <vallentin@icir.org>
2015-07-09 12:41:16 -0700

    Add FreeBSD system status to tmux corner

--- .tmux/stats.sh
+++ .tmux/stats.sh
@@ -5,6 +5,8 @@ if [ "$OS" == "linux-gnu" ]; then
   stats=$(top -b -n 1 | head -n 5)
 elif [ "$OS" == "darwin" ]; then
   stats=$(top -R -F -l 1 | head -n 9)
+elif [ "$OS" == "freebsd" ]; then
+  stats=$(top -I -b | head -n 5)
 fi
 
 printf %s "$stats" | awk -f ~/.tmux/stats-$OS.awk
