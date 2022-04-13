de02ca0930b246a36269111cf86c6bbb47706317
Matthias Vallentin <matthias@vallentin.net>
2022-04-13 06:11:27 +0200

    Add lualine as status line

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -356,6 +356,19 @@ return require('packer').startup(function(use)
     end
   }
 
+  -- A neat status line.
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup() {
+        options = {
+          theme = 'gruvbox',
+        },
+      }
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
