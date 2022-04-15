f316680fb348aa1f3e350d47cbd1ed934c9c3ea3
Matthias Vallentin <matthias@vallentin.net>
2022-04-15 19:40:02 +0200

    Add fish syntax highlighting

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -365,6 +365,9 @@ return require('packer').startup(function(use)
     end
   }
 
+  --- Fish integration.
+  use 'khaveesh/vim-fish-syntax'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
