6b15fc0cb2f02f9b29471d6d6f8c3e05d744a35b
Matthias Vallentin <matthias@vallentin.net>
2022-05-14 21:06:56 +0200

    Style R code with styler

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -290,6 +290,7 @@ return require('packer').startup(function(use)
     config = function()
       require("null-ls").setup({
         sources = {
+          require("null-ls").builtins.formatting.styler,
           require("null-ls").builtins.formatting.stylua,
           require("null-ls").builtins.diagnostics.markdownlint,
           require("null-ls").builtins.diagnostics.shellcheck,
