058748cad50819c70852c67050f3c41ba248525b
Matthias Vallentin <matthias@vallentin.net>
2022-06-06 10:43:50 +0200

    Add LaTeX support

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -394,6 +394,21 @@ return require('packer').startup(function(use)
     end
   }
 
+  -- LaTeX integration.
+  use {
+    'lervag/vimtex',
+    config = function()
+      vim.g.tex_flavor = 'latex'
+      -- Use Skim on macOS. This requires the following Sync settings:
+      --   Preset: Custom
+      --   Command: nvim
+      --   Arguments: --headless -c "VimtexInverseSearch %line '%file'"
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
+    end
+  }
+
   --- R integration.
   use {
     'jalvesaq/Nvim-R',
