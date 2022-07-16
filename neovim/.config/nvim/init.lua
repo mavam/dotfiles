fd2f5f639276f998a5e3334124f1b76dbbc61604
Matthias Vallentin <matthias@vallentin.net>
2022-07-16 16:16:30 +0200

    Support virtual envs with pyright

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -63,6 +63,24 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   })
 end
 
+-- Utility function to find the Python path.
+-- Detail: https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-876700701
+get_python_path = function(workspace)
+  local util = require('lspconfig/util')
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+  -- Find and use virtualenv from pipenv in workspace directory.
+  local match = vim.fn.glob(util.path.join(workspace, 'Pipfile'))
+  if match ~= '' then
+    local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. match .. ' pipenv --venv'))
+    return util.path.join(venv, 'bin', 'python')
+  end
+  -- Fallback to system Python.
+  return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+end
+
 -- Shared on_attach handler for language server tooling.
 custom_on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -276,7 +294,10 @@ return require('packer').startup(function(use)
       }
       -- Python
       require('lspconfig').pyright.setup {
-        on_attach = custom_on_attach
+        on_attach = custom_on_attach,
+        on_init = function(client)
+            client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+        end
       }
       -- R
       require('lspconfig').r_language_server.setup {
