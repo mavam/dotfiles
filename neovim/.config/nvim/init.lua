3cbdd2e8c6e0313d3985a75fd96f59f10a152bce
Matthias Vallentin <matthias@vallentin.net>
2022-08-11 08:59:17 +0200

    Add black for Python formatting

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -295,6 +295,7 @@ return require('packer').startup(function(use)
     config = function()
       require("null-ls").setup({
         sources = {
+          require("null-ls").builtins.formatting.black,
           require("null-ls").builtins.formatting.stylua,
           require("null-ls").builtins.diagnostics.markdownlint,
           require("null-ls").builtins.diagnostics.shellcheck,
