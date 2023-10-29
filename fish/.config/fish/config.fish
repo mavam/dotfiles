7bf92d16544135a43705393707856d25a5a2e819
Matthias Vallentin <matthias@vallentin.net>
2023-10-29 05:52:24 +0100

    Remove inline Kanagawa colorscheme config
    We're currently using catppuccin.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -107,38 +107,6 @@ if status is-interactive
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
   gpgconf --launch gpg-agent
 
-  # Kanagawa color scheme: https://github.com/rebelot/kanagawa.nvim/
-  set -l foreground DCD7BA
-  set -l selection 2D4F67
-  set -l comment 727169
-  set -l red C34043
-  set -l orange FF9E64
-  set -l yellow C0A36E
-  set -l green 76946A
-  set -l purple 957FB8
-  set -l cyan 7AA89F
-  set -l pink D27E99
-  # Syntax Highlighting Colors
-  set -g fish_color_normal $foreground
-  set -g fish_color_command $cyan
-  set -g fish_color_keyword $pink
-  set -g fish_color_quote $yellow
-  set -g fish_color_redirection $foreground
-  set -g fish_color_end $orange
-  set -g fish_color_error $red
-  set -g fish_color_param $purple
-  set -g fish_color_comment $comment
-  set -g fish_color_selection --background=$selection
-  set -g fish_color_search_match --background=$selection
-  set -g fish_color_operator $green
-  set -g fish_color_escape $pink
-  set -g fish_color_autosuggestion $comment
-  # Completion Pager Colors
-  set -g fish_pager_color_progress $comment
-  set -g fish_pager_color_prefix $cyan
-  set -g fish_pager_color_completion $foreground
-  set -g fish_pager_color_description $comment
-
   # Simplify SSH: https://sw.kovidgoyal.net/kitty/kittens/ssh/
   abbr -g s 'kitty +kitten ssh'
 
