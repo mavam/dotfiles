0bd24677c13396529d3378b96546f3363bd35322
Matthias Vallentin <matthias@vallentin.net>
2025-04-26 08:05:29 -0700

    Remove two more plugins

--- neovim/.config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++
@@ -1,5 +0,0 @@
--- Syntax aware text-objects, select, move, swap, and peek support.
-return {
-  'nvim-treesitter/nvim-treesitter-textobjects',
-  dependencies = 'nvim-treesitter/nvim-treesitter'
-}
