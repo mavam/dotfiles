b0f076da81218b454ce6416324af554e487dc379
Matthias Vallentin <matthias@vallentin.net>
2025-10-10 10:53:50 +0200

    Add more paths

--- fish/conf.tools/tenzir.fish
+++ fish/conf.tools/tenzir.fish
@@ -1,3 +1,5 @@
 fish_add_path -p \
+    ~/code/tenzir/tenzir/main/build/release/bin \
     ~/code/tenzir/tenzir/main/build/bin \
+    ~/code/tenzir/tenzir/build/release/bin \
     ~/code/tenzir/tenzir/build/bin
