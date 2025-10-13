8af6d687bd7ee999e0dc5535c8947c01c5a7dbfd
Matthias Vallentin <matthias@vallentin.net>
2025-10-13 18:16:45 +0200

    Simplify tenzir dev paths

--- fish/conf.tools/tenzir.fish
+++ fish/conf.tools/tenzir.fish
@@ -1,5 +1,1 @@
-fish_add_path -p \
-    ~/code/tenzir/tenzir/main/build/release/bin \
-    ~/code/tenzir/tenzir/main/build/bin \
-    ~/code/tenzir/tenzir/build/release/bin \
-    ~/code/tenzir/tenzir/build/bin
+fish_add_path -p ~/code/tenzir/tenzir/main/build/*/release/bin
