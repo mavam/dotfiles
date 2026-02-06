204ec0d496d86430a46ad8bd8262631cea5aaee3
Matthias Vallentin <matthias@vallentin.net>
2026-02-06 08:05:14 +0100

    Fix new worktree layout

--- fish/conf.tools/tenzir.fish
+++ fish/conf.tools/tenzir.fish
@@ -1,4 +1,4 @@
-set -l tenzir_bin ~/code/tenzir/tenzir/main/build/*/release/bin
+set -l tenzir_bin ~/code/tenzir/tenzir/build/*/release/bin
 if test -n "$tenzir_bin[1]" -a -d "$tenzir_bin[1]"
     fish_add_path -p $tenzir_bin
 end
