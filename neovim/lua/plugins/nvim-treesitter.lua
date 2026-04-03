60c72c3f5ff9135be1842707daaf9b6eaef60052
Matthias Vallentin <matthias.vallentin@tenzir.com>
2026-04-03 10:56:45 -0700

    Update nvim-treesitter for Neovim 0.12
    The nvim-treesitter plugin switched its default branch from master
    to main with a completely new API for Neovim 0.12. This adapts the
    config accordingly:
    
    - Pin nvim-treesitter to the main branch
    - Replace removed configs/parsers API with new setup pattern
    - Use vim.treesitter.start() for highlighting via FileType autocmd
    - Register TQL parser via User TSUpdate autocmd
    - Drop nvim-treesitter-refactor (incompatible with new API)
    - Drop legacy TS highlight groups and incremental selection

--- neovim/lua/plugins/nvim-treesitter.lua
+++ neovim/lua/plugins/nvim-treesitter.lua
@@ -1,60 +1,53 @@
--- TreeSitter integration, and additional textobjects for it.
+-- TreeSitter integration.
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  lazy = false,
   build = ':TSUpdate',
   dependencies = {
     'tenzir/tree-sitter-tql',
-    'nvim-treesitter/nvim-treesitter-refactor',
   },
   config = function()
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_config.tql = parser_config.tql or {}
-    parser_config.tql.install_info = parser_config.tql.install_info or {
-      url = 'https://github.com/tenzir/tree-sitter-tql',
-      files = { 'src/parser.c' },
-      branch = 'main',
-    }
-    parser_config.tql.filetype = 'tql'
+    -- Register the custom TQL parser.
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'TSUpdate',
+      callback = function()
+        require('nvim-treesitter.parsers').tql = {
+          install_info = {
+            url = 'https://github.com/tenzir/tree-sitter-tql',
+            branch = 'main',
+          },
+        }
+      end,
+    })
 
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
-        'bash',
-        'c',
-        'comment',
-        'cpp',
-        'fish',
-        'json',
-        'lua',
-        'markdown',
-        'python',
-        'r',
-        'tql',
-        'yaml',
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-      },
-      refactor = {
-        highlight_definitions = { enable = true },
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          scope_incremental = '<CR>',
-          node_incremental = '<TAB>',
-          node_decremental = '<S-TAB>',
-        },
-      },
+    -- Install parsers (no-op if already installed).
+    require('nvim-treesitter').install {
+      'bash',
+      'c',
+      'comment',
+      'cpp',
+      'fish',
+      'json',
+      'lua',
+      'markdown',
+      'python',
+      'r',
+      'tql',
+      'yaml',
     }
+
+    -- Enable treesitter highlighting for supported filetypes.
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        'bash', 'sh', 'c', 'cpp', 'fish', 'json', 'lua', 'markdown',
+        'python', 'r', 'tql', 'yaml',
+      },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
 
     vim.filetype.add({ extension = { tql = 'tql' } })
-
-    local set_hl = vim.api.nvim_set_hl
-    set_hl(0, 'TSDefinition', { underline = true })
-    set_hl(0, 'TSDefinitionUsage', { underline = true })
-    set_hl(0, 'TSCurrentScope', { link = 'Normal' })
-    set_hl(0, 'TSDefinitionUsage', { link = 'TSDefinition' })
   end,
 }
